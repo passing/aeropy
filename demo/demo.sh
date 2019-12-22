a68071dab776c6b71395ff5e63e25de66361eb82
Martin Passing <github@passing.name>
2019-12-22 18:02:18 +0100

    demo files

---
+++ demo/demo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+../aeropy.py -input demo.glo -number 3 -png demo1.png -png-resolution 7 -png-stretch 5 -output demo
+../aeropy.py -input demo.glo -number 3 -png demo2.png -png-resolution 8 -png-stretch 5
